1cddf4c8caadd1ae48ebfc79b50cf4d4b01609e6
Soukaina-mehdi <mchiheb.lking@gmail.com>
2026-08-07 18:41:23 +0100

    Add main menu logic

---
+++ scripts/main_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _ready():
+    $PlayButton.pressed.connect(start_game)
+    $QuitButton.pressed.connect(quit_game)
+
+func start_game():
+    get_tree().change_scene_to_file("res://levels/level_01.tscn")
+
+func quit_game():
+    get_tree().quit()
