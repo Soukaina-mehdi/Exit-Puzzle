28e23eaf86354b4d32812a1a95c3db7e11ae6c14
Soukaina-mehdi <mchiheb.lking@gmail.com>
2026-08-07 18:42:38 +0100

    Connect mobile controls to player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,22 @@
 extends CharacterBody2D
 
 @export var speed := 250.0
+var mobile_direction := Vector2.ZERO
+
+func _ready():
+    var controls = get_tree().get_first_node_in_group("mobile_controls")
+    if controls:
+        controls.move_input.connect(set_mobile_direction)
 
 func _physics_process(_delta):
-    var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+    var keyboard_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+    var direction = keyboard_direction
+    
+    if direction == Vector2.ZERO:
+        direction = mobile_direction
+    
     velocity = direction * speed
     move_and_slide()
+
+func set_mobile_direction(value: Vector2):
+    mobile_direction = value
