af14fdd663fbda5ba1fc56faa2d6f3bd15cae8eb
Soukaina-mehdi <mchiheb.lking@gmail.com>
2026-08-07 18:42:57 +0100

    Connect mobile controls signals and register group

--- scripts/mobile_controls.gd
+++ scripts/mobile_controls.gd
@@ -4,6 +4,13 @@ var direction := Vector2.ZERO
 
 signal move_input(value)
 
+func _ready():
+    add_to_group("mobile_controls")
+    $Up.pressed.connect(_on_up_pressed)
+    $Down.pressed.connect(_on_down_pressed)
+    $Left.pressed.connect(_on_left_pressed)
+    $Right.pressed.connect(_on_right_pressed)
+
 func _on_up_pressed():
     move_input.emit(Vector2.UP)
 
